035f57002693d78b6137bf624b8d3927f5a0fcd3
Shilpi Mishra <shilpivats111@gmail.com>
2023-12-30 23:44:11 +0530

    15th Dec Class

--- 14th_Dec_tutorial/14index.js
+++ 14th_Dec_tutorial/14index.js
@@ -1,64 +1,138 @@
-// const apiKey = '21246e7eca571f524c32d3ae43f72a01';
-// const movieUrl = 'http://api.themoviedb.org/3/movie/';
-// const personUrl = 'http://api.themoviedb.org/3/person/';
-// const imageUrl = 'http://image.tmdb.org/t/p/original/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg'; 
-
-
 let data
+const apiKey = '21246e7eca571f524c32d3ae43f72a01';
+const movieUrl = 'http://api.themoviedb.org/3/movie/';
+const personUrl = 'http://api.themoviedb.org/3/person/';
+const imageUrl = 'http://image.tmdb.org/t/p/original';
+
+let movieIds;  
+const queryString = window.location.search;
+const queryParamsMap = new URLSearchParams(queryString);
+console.log(queryParamsMap.get('id'), queryParamsMap.get('posterPath'));
+
 
 import('/src/moviesPlay.js')
-	.then(res => {
-		console.log('data imported into data constant');
-		data = res;
-		run();
-	});
+    .then(res => {
+        console.log('data imported into data constant');
+        data = res;
+        if(queryString){
+            showMovie(queryParamsMap.get('id'), queryParamsMap.get('posterPath'));
+        }
+        run();
+    });
 
 function run() {
-    const filteredMovies = data.movies.filter(movie =>{
+    const filteredMovies = data.movies.filter(movie => {
         return movie.runtimeMinutes > 150;
     })
-    // console.log(filteredMovies.length);
+
     const totalRuntime = filteredMovies.reduce((acc, movie) => {
         console.log(movie.runtimeMinutes);
         return acc + movie.runtimeMinutes;
     }, 0)
-    console.log('Total RunTime: ' + totalRuntime + ', Avg RunTime: ' + Math.ceil(totalRuntime/filteredMovies.length));
+    console.log('Total RunTime: ' + totalRuntime + ', Avg RunTime: ' + Math.ceil(totalRuntime / filteredMovies.length));
 
-    // Reformat the filtered output:
-    const output = filteredMovies.map(movie => {
-        return {
-            title: movie.title,
-            releaseDate: movie.releaseDate,
-            runtimeMinutes: movie.runtimeMinutes,
-            id: movie.tmdbId
-        }
-    })
-    console.log(output);
+    // Reformat the filtered output to just get movie id's:
+    movieIds = filteredMovies.map(movie => movie.tmdbId);
+    console.log(movieIds);
+    // getMovieInformation(movieIds);
 }
 
 function getMovieInformation() {
-    const apiKey = '21246e7eca571f524c32d3ae43f72a01';
-    const movieUrl = 'http://api.themoviedb.org/3/movie/';
-    const movieId = '98';
-    const imageUrl = 'http://image.tmdb.org/t/p/original';
+    const fetchArray = movieIds.map(movieId => {
+        return (
+            fetch(`${movieUrl}${movieId}?api_key=${apiKey}`)
+                .then(response => response.json())
+        )
+    });
+    Promise.all(fetchArray)
+        .then(fetchResponses => {
+            const moviesInfo = fetchResponses.map(resp => {
+                return {
+                    id: resp.id, overview: resp.overview,
+                    posterPath: resp.poster_path, releaseDate: resp.release_date,
+                    runTime: resp.runtime, tagLine: resp.tagline,
+                    title: resp.title
+                }
+            })
+            console.log(moviesInfo);
+            document.getElementById('content').innerHTML = getMovieHtml(moviesInfo);
+        })
 
-    const fetchUrl = `${movieUrl}${movieId}?api_key=${apiKey}`;
-    console.log('Calling fetch');
-    const startTime = Date.now();
-    let endTime;
-    fetch(fetchUrl)
-     .then(response => {
-        endTime = Date.now();
-        console.log('>>>>> Got Response. Time taken: ' + (endTime - startTime) + 'milliseconds');
-        return response.json()
-     })
-     .then(movie => {
-        // console.log(movie);
-        console.log(movie.backdrop_path, movie.poster_path);
-        const htmlContent = `<img src='${imageUrl}${movie.poster_path}' />`
-        document.getElementById('content').innerHTML = htmlContent;
-     })
-    endTime = Date.now();
-    console.log('Finished calling fetch. Time taken: ' + (endTime - startTime) + ' milliseconds');
 }
 
+function getMovieHtml(moviesInfo) {
+    let movieHtml = '<div class="ui link cards">'
+
+
+
+    const movieCards = moviesInfo.reduce((html, movie) => {
+        return html + `
+            <div class="card">
+            <div class="image">
+            <a href='./movies.html?id=${movie.id}&posterPath=${movie.posterPath}'>
+            <img src='${imageUrl}${movie.posterPath}' />
+            </a>
+            </div>
+            <div class="content">
+            <div class="header">${movie.title}</div>
+            <div class="meta">
+                <a>${movie.releaseDate}</a>
+            </div>
+            <div class="description">
+                ${movie.tagLine}
+            </div>
+            </div>
+        </div>
+  `
+    }, '');
+
+    movieHtml += `${movieCards}</div>`;
+    console.log(movieHtml);
+    return movieHtml;
+}
+
+function showMovie(id, posterPath) {
+    const movieInfo = data.movies.find(movie => {
+        return movie.tmdbId === id;
+    })
+    getCastHtml(movieInfo.cast)
+      .then(castHtml => {
+        document.getElementById('castInfo').innerHTML= castHtml;
+      })
+
+    document.getElementById('title').innerHTML= movieInfo.title;
+    document.getElementById('overview').innerHTML= movieInfo.overview;
+    document.getElementById('moviePoster').innerHTML= `<img src='${imageUrl}${posterPath}' />`
+}
+
+async function getCastHtml(cast){
+    const castFetchArray = cast.map(cm => {
+        return (
+            fetch(`${personUrl}${cm.id}?api_key=${apiKey}`)
+             .then(response => response.json())
+        )
+    });
+    const castResponses = await Promise.all(castFetchArray);
+
+     
+        let castHtml = '<div class="ui cards">'
+        castResponses.forEach(cr => {
+            castHtml+= `
+                <div class="card">
+                    <div class="content">
+                    <img class="right floated mini ui image" src="${imageUrl}${cr.profile_path}">
+                    <div class="header">
+                        ${cr.name}
+                    </div>
+                    <div class="meta">
+                        ${cr.birthday}
+                    </div>
+                   </div>
+                </div>
+            `
+        })
+        castHtml+='</div>';
+        
+
+        return castHtml;
+}
